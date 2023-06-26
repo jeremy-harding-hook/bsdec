dab7b1b9ac235fa554bc07237a98e83babfe7330
Jeremy Harding Hook <jeremyhhook@gmail.com>
2023-06-26 18:19:45 +0200

    implemented input validation and accessing of the input assembly, as well as the "help" functionality.

---
+++ BsdecSchemaGen/Functions/Help.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BsdecSchemaGen.Functions
+{
+    internal static class Help
+    {
+        private static readonly string errorText =
+            $"Usage: {Program.ProgramFileName} [options] class_name assembly_file\n\n" +
+            $"A tool for generating a savefile schema based on an assembly. This schema\n" +
+            $"can then be used in conjunction with bsdec to permit manual reading and writing\n" +
+            $"of savefiles.\n" +
+            $"Note that this program is only compatible with .net assemblies in CIL.\n" +
+            $"If obfuscation has been applied, you may need to undo that first.\n\n" +
+            $"Options:\n" +
+            $"    -r reader\n" +
+            $"    --reader reader\n" +
+            $"        Specifies the name of the reader method. At least one of -r, -w must be \n" +
+            $"        set.\n\n" +
+            $"    -w writer\n" +
+            $"    --writer writer\n" +
+            $"        Specifies the name of the writer method. At least one of -r, -w must be\n" +
+            $"        set.\n\n" +
+            $"    -h\n" +
+            $"    --help\n" +
+            $"        Returns this help text.\n\n" +
+            $"If there are extra parameters to pass into the reader and writer methods, that\n" +
+            $"can be achieved by appending them to the method name like so:\n" +
+            "    -r reader_method_name+param_name_1=some_value+param_name_2=another_value\n" +
+            "Note this only works with parameters of primitive type.\n\n" +
+            $"Parameters:\n" +
+            $"    class_name:\n" +
+            $"        The name of the top-level class corresponding to the object being saved,\n" +
+            $"        fully qualified with namespace. If no fully-qualified match exists, the\n" +
+            $"        program will instead treat the class_name as partially qualified and will\n" +
+            $"        try to make it work.\n\n" +
+            $"    assembly_file:\n" +
+            $"        The path to the assembly containing the saving code for saving.\n\n" +
+            $"Exit values:\n" +
+            $"    0: Successfully executed.\n" +
+            $"    1: Generic error prevented the schema from being generated.\n" +
+            $"    2: Bad usage.";
+
+        public static void ShowHelpText(bool stderr)
+        {
+            if(stderr)
+                Console.Error.WriteLine(errorText);
+            else
+                Console.WriteLine(errorText);
+        }
+    }
+}
--- BsdecSchemaGen/Program.cs
+++ BsdecSchemaGen/Program.cs
@@ -1,17 +1,173 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using BsdecSchemaGen.Functions;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace BsdecSchemaGen
 {
-    internal class Program
+    internal partial class Program
     {
+        internal static string? ProgramFileName { get; private set; }
+#if RELEASELINUX
+        internal const string ShippedProgramFilename = "bsdec-schema-gen";
+#else
+        internal const string ShippedProgramFilename = "BsdecSchemaGen.exe";
+#endif
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.Error.WriteLine("Hi! Here's an error!");
-            Task.Delay(10000).Wait();
-            Console.WriteLine("Hello again!");
-            Console.Error.WriteLine("Uh-oh, another error!");
+            try
+            {
+                ProgramFileName = Path.GetFileName(Environment.ProcessPath);
+                if (string.IsNullOrEmpty(ProgramFileName))
+                    ProgramFileName = ShippedProgramFilename;
+
+                int returnValue = HandleArgs(args.ToList());
+                Environment.Exit(returnValue);
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine($"Unhandled exception: {ex.Message}");
+                Environment.Exit(1);
+            }
         }
+
+        static int HandleArgs(List<string> args)
+        {
+            Option helpOption = new("help", 'h');
+            Option readerOption = new("reader", 'r');
+            Option writerOption = new("writer", 'w');
+            List<Option> options = new()
+            {
+                helpOption,
+                readerOption,
+                writerOption
+            };
+            string? className = null;
+            string? assembly = null;
+            string? readerName = null;
+            string? writerName = null;
+            Dictionary<string, string> readerParams = new ();
+            Dictionary<string, string> writerParams = new();
+
+            Regex multiflagFinder = shortFlags();
+            Regex longFlagFinder = longFlags();
+            foreach (string arg in args)
+            {
+                if (multiflagFinder.IsMatch(arg))
+                {
+                    foreach (char flag in arg[1..])
+                    {
+                        if (!TryMatchFlag(options, flag))
+                            return FailHelpfully();
+                    }
+                }
+                else if (longFlagFinder.IsMatch(arg))
+                {
+                    if (!TryMatchLongformOption(options, arg[2..]))
+                        return FailHelpfully();
+                }
+                else if (readerOption.set && readerName == null)
+                {
+                    if(!TryParseIoMethod(arg, readerParams, out readerName)) 
+                        return 2;
+                }
+                else if (writerOption.set && writerName == null)
+                {
+                    if(!TryParseIoMethod(arg, writerParams, out writerName))
+                        return 2;
+                }
+                else if (className == null)
+                {
+                    className = arg;
+                }
+                else if (assembly == null)
+                {
+                    assembly = arg;
+                }
+                else
+                {
+                    return FailHelpfully();
+                }
+            }
+
+            if (helpOption.set)
+            {
+                Help.ShowHelpText(false);
+                return 0;
+            }
+
+            return SchemaGenerator.Run(assembly, className, readerName, writerName);
+        }
+
+        private static int FailHelpfully()
+        {
+            Help.ShowHelpText(true);
+            return 2;
+        }
+
+        private static bool TryMatchFlag(List<Option> options, char flag)
+        {
+            for (int i = 0; i < options.Count; i++)
+            {
+                if (flag == options[i].flag)
+                {
+                    options[i].set = true;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private static bool TryMatchLongformOption(List<Option> options, string longformOption)
+        {
+            for (int i = 0; i < options.Count; i++)
+            {
+                if (longformOption == options[i].name)
+                {
+                    options[i].set = true;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private static bool TryParseIoMethod(string commandLineArg, Dictionary<string,string> parameterDictionary, out string methodName)
+        {
+            string[] methodParameters = commandLineArg.Split('+');
+            methodName = methodParameters[0];
+            for (int i = 1; i < methodParameters.Length; i++)
+            {
+                string[] splitParameter = methodParameters[i].Split('=',2);
+                if(splitParameter.Length != 2)
+                {
+                    Console.Error.WriteLine($"Invalid method parameter syntax: {methodParameters[i]}");
+                    Console.Error.WriteLine($"See '{Program.ProgramFileName}' --help for correct usage.");
+                    return false;
+                }
+                parameterDictionary.Add(splitParameter[0], splitParameter[1]);
+            }
+            return true;
+        }
+
+        class Option
+        {
+            public bool set;
+            public string? name;
+            public char? flag;
+            public Option(string? name, char? flag)
+            {
+                this.name = name;
+                this.flag = flag;
+            }
+        }
+
+        [GeneratedRegex("^-[a-zA-Z]+$")]
+        private static partial Regex shortFlags();
+
+        [GeneratedRegex("^--[a-zA-Z]+$")]
+        private static partial Regex longFlags();
     }
 }
