3cf7b2fc90cabdd9efe03dd481c083127f0cdf5c
Jeremy Harding Hook <jeremyhhook@gmail.com>
2023-07-20 19:36:24 +0200

    Added copyright notices

--- BsdecSchemaGen/AssemblyBuilder/TypeBuilder.cs
+++ BsdecSchemaGen/AssemblyBuilder/TypeBuilder.cs
@@ -1,4 +1,25 @@
-﻿using Mono.Cecil;
+﻿//-----------------------------------------------------------------------
+//
+// Copyright 2023 Jeremy Harding Hook
+//
+// This file is part of BsdecSchemaGen.
+//
+// BsdecSchemaGen is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free
+// Software Foundation, either version 3 of the License, or (at your option)
+// any later version.
+//
+// BsdecSchemaGen is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+// details.
+//
+// You should have received a copy of the GNU General Public License along with
+// BsdecSchemaGen. If not, see <https://www.gnu.org/licenses/>.
+//
+//-----------------------------------------------------------------------
+
+using Mono.Cecil;
 using System;
 using System.Collections.Generic;
 using System.Linq;
